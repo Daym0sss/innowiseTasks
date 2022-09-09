78cab698123413e59bec065c828adfb0e590c6f3
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-10 01:39:55 +0300

    add Task1.php

---
+++ Task1.php
@@ -0,0 +1,38 @@
+<?php
+namespace src;
+
+    class Task1
+    {
+        static function check($number) : ?string
+        {
+            if (is_int($number))
+            {
+                return ($number>30) ? "More than 30" : (($number>20) ? "More than 20" : (($number>10) ? "More than 10" : "Equal or less than 10"));
+            }
+            else
+            {
+                throw new \InvalidArgumentException('Выброшено исключение: введённое значение должно быть целым числом');
+            }
+        }
+
+        static function main($number) : ?string
+        {
+            $result=null;
+            try
+            {
+                $result=Task1::check($number);
+            }
+            catch (\Exception $e)
+            {
+                echo $e->getMessage();
+            }
+            finally
+            {
+                return $result;
+            }
+        }
+    }
+
+    $task=Task1::main(2);
+    echo $task;
+?>
