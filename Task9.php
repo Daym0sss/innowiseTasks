ea2d352ea66ac8b8335a61d5f6f0e1f399bb502f
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-10 01:51:44 +0300

    add Task9.php

---
+++ Task9.php
@@ -0,0 +1,62 @@
+<?php
+namespace src;
+
+class Task9
+{
+    static function check($arr,$n) : ?array
+    {
+        $result=array();
+        if (!is_array($arr) or count($arr)==0)
+        {
+            throw new \InvalidArgumentException("Первый параметр должен быть массивом ненулевой длины");
+        }
+        else if (count($arr)<3)
+        {
+            throw new \InvalidArgumentException("Первый параметр должен быть массивом длины от 3 и более");
+        }
+        else
+        {
+            $i=0;
+            while ($i<=count($arr)-3)
+            {
+                if ($arr[$i]+$arr[$i+1]+$arr[$i+2]==$n)
+                {
+                    array_push($result,$arr[$i] . ' + ' . $arr[$i+1] . ' + ' . $arr[$i+2] . ' = ' . $n);
+                }
+                $i++;
+            }
+            return $result;
+        }
+    }
+
+    static function main($arr,$n) : ?array
+    {
+        $result=null;
+        try
+        {
+            $result=Task9::check($arr,$n);
+        }
+        catch (\Exception $e)
+        {
+            echo $e->getMessage();
+        }
+        finally
+        {
+            return $result;
+        }
+    }
+}
+
+    $arr=array();
+    array_push($arr,1);
+    array_push($arr,2);
+    array_push($arr,3);
+    array_push($arr,1);
+    array_push($arr,2);
+    array_push($arr,3);
+
+    $n=6;
+    $task=Task9::main($arr,$n);
+    echo "<pre>";
+    print_r($task);
+    echo "</pre>";
