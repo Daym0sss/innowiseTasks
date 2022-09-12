89053cd9af8bf40de1bb42f5ec4982ecf2b53484
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-12 19:07:46 +0300

    Updated with CS Fixer

--- Task10.php
+++ Task10.php
@@ -1,49 +1,39 @@
 <?php
+
 namespace src;
 
 class Task10
 {
-    static function check($num) : ?array
+    public function check(int $num): array
     {
-        $result=array();
-        if (is_int($num))
-        {
-            array_push($result,$num);
-            while($num!=1)
-            {
-                if ($num % 2==0)
-                {
-                    $num=$num/2;
+        $result = [];
+        if (is_int($num)) {
+            array_push($result, $num);
+            while ($num != 1) {
+                if ($num % 2 == 0) {
+                    $num = $num / 2;
+                } else {
+                    $num = $num * 3 + 1;
                 }
-                else
-                {
-                    $num=$num*3+1;
-                }
-                array_push($result,$num);
+                array_push($result, $num);
             }
+
             return $result;
-        }
-        else
-        {
-            throw new \InvalidArgumentException("Введённое значение должно быть целым числом");
+        } else {
+            throw new \InvalidArgumentException('Введённое значение должно быть целым числом');
         }
     }
 
-    static function main($num) : ?array
+    public function main(int $num): array
     {
-        $result=null;
-        try
-        {
-            $result=Task10::check($num);
-        }
-        catch (\Exception $e)
-        {
+        $result = null;
+
+        try {
+            $result = $this->check($num);
+        } catch (\Exception $e) {
             echo $e->getMessage();
-        }
-        finally
-        {
+        } finally {
             return $result;
         }
     }
-
 }
