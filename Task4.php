bec916b451da1eae4b87e7f59827727102a3ece7
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-12 18:31:15 +0300

    Updated with CS FIxer

--- Task4.php
+++ Task4.php
@@ -1,65 +1,52 @@
 <?php
+
 namespace src;
 
 class Task4
 {
-    static function check($string) : ?string
+    public function check(string $string): string
     {
-        $error=false;
-        $i=0;
-        while ($i<strlen($string) and !$error)
-        {
-            if (!ctype_alpha($string[$i]))
-            {
-                if ($string[$i] != '_' and $string[$i] != '-' and $string[$i] != ' ')
-                {
+        $error = false;
+        $i = 0;
+        while ($i < strlen($string) and !$error) {
+            if (!ctype_alpha($string[$i])) {
+                if ($string[$i] != '_' and $string[$i] != '-' and $string[$i] != ' ') {
                     $error = true;
                 }
             }
             $i++;
         }
-        if (!$error)
-        {
-            $i=0;
-            $result="";
-            while ($i<strlen($string))
-            {
-                if ($string[$i]==' ' or $string[$i]=='_' or $string[$i]=='-')
-                {
+        if (!$error) {
+            $i = 0;
+            $result = '';
+            while ($i < strlen($string)) {
+                if ($string[$i] == ' ' or $string[$i] == '_' or $string[$i] == '-') {
                     $i++;
-                }
-                else
-                {
-                    $result.=strtoupper($string[$i]);
+                } else {
+                    $result .= strtoupper($string[$i]);
                     $i++;
-                    while($i<strlen($string) and $string[$i]!=' ' and $string[$i]!='_' and $string[$i]!='-')
-                    {
-                        $result.=$string[$i];
+                    while ($i < strlen($string) and $string[$i] != ' ' and $string[$i] != '_' and $string[$i] != '-') {
+                        $result .= $string[$i];
                         $i++;
                     }
                 }
             }
+
             return $result;
-        }
-        else
-        {
+        } else {
             throw new \InvalidArgumentException('Выброшено исключение: введённая строка должна содержать только символы A-Z a-z, символы _ и - и пробелы');
         }
     }
 
-    static function main($string) : ?string
+    public function main(string $string): string
     {
-        $result=null;
-        try
-        {
-            $result=Task4::check($string);
-        }
-        catch (\Exception $e)
-        {
+        $result = null;
+
+        try {
+            $result = $this->check($string);
+        } catch (\Exception $e) {
             echo $e->getMessage();
-        }
-        finally
-        {
+        } finally {
             return $result;
         }
     }
