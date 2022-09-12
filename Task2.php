d14b5abce89013abc50e9eff5d5d2acc2d624dfc
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-12 14:22:05 +0300

    Delete method call

---
+++ Task2.php
@@ -0,0 +1,44 @@
+<?php
+namespace src;
+
+class Task2
+{
+    static function countDays($date) : ?int
+    {
+        if (preg_match("/^(0[1-9]|[12][0-9]|3[01])[-](0[1-9]|1[012])[-](19|20)\d\d$/",$date))
+        {
+            $today=strtotime('now');
+            $birthday=strtotime($date);
+            $diff=round(($birthday-$today)/(3600*24),1);
+            if ($diff<0)
+            {
+                return abs($diff);
+            }
+            else
+            {
+                return $diff+1;
+            }
+        }
+        else
+        {
+            throw new \InvalidArgumentException('Выброшено исключение: введённое значение даты должно соответствовать формату DD-MM-YYYY');
+        }
+    }
+
+    static function main($date): ?int
+    {
+        $result=null;
+        try
+        {
+            $result=Task2::countDays($date);
+        }
+        catch (\Exception $e)
+        {
+            echo $e->getMessage();
+        }
+        finally
+        {
+            return $result;
+        }
+    }
+}
