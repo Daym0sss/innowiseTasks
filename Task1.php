c59eb26350ed4bade7eb754c4109f1d75399c909
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-12 16:49:35 +0300

    Update Task1.php

--- Task1.php
+++ Task1.php
@@ -1,34 +1,28 @@
 <?php
+
 namespace src;
 
-    class Task1
+class Task1
+{
+    public function check(int $number): string
     {
-        public function check(int $number) : string
-        {
-            if (is_int($number))
-            {
-                return ($number>30) ? "More than 30" : (($number>20) ? "More than 20" : (($number>10) ? "More than 10" : "Equal or less than 10"));
-            }
-            else
-            {
-                throw new \InvalidArgumentException('Выброшено исключение: введённое значение должно быть целым числом');
-            }
-        }
-
-        public function main(int $number) : string
-        {
-            $result=null;
-            try
-            {
-                $result=$this->check($number);
-            }
-            catch (\Exception $e)
-            {
-                echo $e->getMessage();
-            }
-            finally
-            {
-                return $result;
-            }
+        if (is_int($number)) {
+            return ($number > 30) ? 'More than 30' : (($number > 20) ? 'More than 20' : (($number > 10) ? 'More than 10' : 'Equal or less than 10'));
+        } else {
+            throw new \InvalidArgumentException('Выброшено исключение: введённое значение должно быть целым числом');
         }
     }
+
+    public function main(int $number): string
+    {
+        $result = null;
+
+        try {
+            $result = $this->check($number);
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        } finally {
+            return $result;
+        }
+    }
+}
