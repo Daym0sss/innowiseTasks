0ee50b2a7d05b68a2bf4123ebbfaf133ca55935f
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-12 19:05:32 +0300

    Updated with CS Fixer

--- Task9.php
+++ Task9.php
@@ -1,47 +1,40 @@
 <?php
+
 namespace src;
 
 class Task9
 {
-    static function check($arr,$n) : ?array
+    public function check(array $arr, int $n): array
     {
-        $result=array();
-        if (!is_array($arr) or count($arr)==0)
-        {
-            throw new \InvalidArgumentException("Первый параметр должен быть массивом ненулевой длины");
-        }
-        else if (count($arr)<3)
-        {
-            throw new \InvalidArgumentException("Первый параметр должен быть массивом длины от 3 и более");
-        }
-        else
-        {
-            $i=0;
-            while ($i<=count($arr)-3)
-            {
-                if ($arr[$i]+$arr[$i+1]+$arr[$i+2]==$n)
-                {
-                    array_push($result,$arr[$i] . ' + ' . $arr[$i+1] . ' + ' . $arr[$i+2] . ' = ' . $n);
+        $result = [];
+        if (!is_array($arr) or count($arr) == 0) {
+            throw new \InvalidArgumentException('Первый параметр должен быть массивом ненулевой длины');
+        } elseif (count($arr) < 3) {
+            throw new \InvalidArgumentException('Первый параметр должен быть массивом длины от 3 и более');
+        } elseif (!is_int($n)) {
+            throw new \InvalidArgumentException('Второй параметр должен быть целым числом');
+        } else {
+            $i = 0;
+            while ($i <= count($arr) - 3) {
+                if ($arr[$i] + $arr[$i + 1] + $arr[$i + 2] == $n) {
+                    array_push($result, $arr[$i] . ' + ' . $arr[$i + 1] . ' + ' . $arr[$i + 2] . ' = ' . $n);
                 }
                 $i++;
             }
+
             return $result;
         }
     }
 
-    static function main($arr,$n) : ?array
+    public function main(array $arr, int $n): array
     {
-        $result=null;
-        try
-        {
-            $result=Task9::check($arr,$n);
-        }
-        catch (\Exception $e)
-        {
+        $result = null;
+
+        try {
+            $result = $this->check($arr, $n);
+        } catch (\Exception $e) {
             echo $e->getMessage();
-        }
-        finally
-        {
+        } finally {
             return $result;
         }
     }
