e2ac8ca283893b72d54d814ddc64d4087a463779
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-12 18:49:37 +0300

    Updated with CS Fixer

--- Task7.php
+++ Task7.php
@@ -1,39 +1,32 @@
 <?php
-    namespace src;
 
-    class Task7
+namespace src;
+
+class Task7
+{
+    public function check(array $arr, int $n): array
     {
-        static function check($arr,$n) : ?array
-        {
-            if (!is_array($arr) or count($arr)==0)
-            {
-                throw new \InvalidArgumentException("Первый параметр должен быть массивом ненулевой длины");
-            }
-            else if ($n<0 or $n>=count($arr))
-            {
-                throw new \InvalidArgumentException("Второй параметр должен быть целым числом от 0 до (длина массива - 1)");
-            }
-            else
-            {
-                array_splice($arr,$n,1);
-                return $arr;
-            }
-        }
+        if (!is_array($arr) or count($arr) == 0) {
+            throw new \InvalidArgumentException('Первый параметр должен быть массивом ненулевой длины');
+        } elseif ($n < 0 or $n >= count($arr)) {
+            throw new \InvalidArgumentException('Второй параметр должен быть целым числом от 0 до (длина массива - 1)');
+        } else {
+            array_splice($arr, $n, 1);
 
-        static function main($arr,$n) : ?array
-        {
-            $result=null;
-            try
-            {
-                $result=Task7::check($arr,$n);
-            }
-            catch (\Exception $e)
-            {
-                echo $e->getMessage();
-            }
-            finally
-            {
-                return $result;
-            }
+            return $arr;
         }
     }
+
+    public function main(array $arr, int $n): array
+    {
+        $result = null;
+
+        try {
+            $result = $this->check($arr, $n);
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        } finally {
+            return $result;
+        }
+    }
+}
