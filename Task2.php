bb7df8967a3875c37e899b2005794e1ca98b0a0c
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-12 18:26:49 +0300

    Updated with CS Fixer

--- Task2.php
+++ Task2.php
@@ -1,43 +1,34 @@
 <?php
+
 namespace src;
 
 class Task2
 {
-    static function countDays($date) : ?int
+    public function countDays(string $date): int
     {
-        if (preg_match("/^(0[1-9]|[12][0-9]|3[01])[-](0[1-9]|1[012])[-](19|20)\d\d$/",$date))
-        {
-            $today=strtotime('now');
-            $birthday=strtotime($date);
-            $diff=round(($birthday-$today)/(3600*24),1);
-            if ($diff<0)
-            {
+        if (preg_match("/^(0[1-9]|[12][0-9]|3[01])[-](0[1-9]|1[012])[-](19|20)\d\d$/", $date)) {
+            $today = strtotime('now');
+            $birthday = strtotime($date);
+            $diff = round(($birthday - $today) / (3600 * 24), 1);
+            if ($diff < 0) {
                 return abs($diff);
+            } else {
+                return $diff + 1;
             }
-            else
-            {
-                return $diff+1;
-            }
-        }
-        else
-        {
+        } else {
             throw new \InvalidArgumentException('Выброшено исключение: введённое значение даты должно соответствовать формату DD-MM-YYYY');
         }
     }
 
-    static function main($date): ?int
+    public function main(string $date): int
     {
-        $result=null;
-        try
-        {
-            $result=Task2::countDays($date);
-        }
-        catch (\Exception $e)
-        {
+        $result = null;
+
+        try {
+            $result = $this->countDays($date);
+        } catch (\Exception $e) {
             echo $e->getMessage();
-        }
-        finally
-        {
+        } finally {
             return $result;
         }
     }
