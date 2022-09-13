148c11582ab7d60d47d76ef79bb0c8b5d930b496
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-13 16:45:27 +0300

    Update Task3.php

--- Task3.php
+++ Task3.php
@@ -4,26 +4,30 @@ namespace src;
 
 class Task3
 {
-    public function check(int $number): int
+    public function check($number): int
     {
         if (is_int($number)) {
-            while (strlen($number) > 1) {
-                $digits = str_split($number);
-                $number = 0;
-                foreach ($digits as $digit) {
-                    $number += $digit;
+            if ($number > 0) {
+                while (strlen($number) > 1) {
+                    $digits = str_split($number);
+                    $number = 0;
+                    foreach ($digits as $digit) {
+                        $number += $digit;
+                    }
                 }
-            }
 
-            return $number;
+                return $number;
+            } else {
+                throw new \InvalidArgumentException('Выброшено исключение: введённое значение должно быть целым положитенльным числом');
+            }
         } else {
             throw new \InvalidArgumentException('Выброшено исключение: введённое значение должно быть целым числом');
         }
     }
 
-    public function main(int $number): int
+    public function main($number): int
     {
-        $result = null;
+        $result = 0;
 
         try {
             $result = $this->check($number);
