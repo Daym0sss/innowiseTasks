f7d4ab9668aee190a6934ad346df54e8d2d38178
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-09-13 17:43:57 +0300

    Update Task10.php

--- Task10.php
+++ Task10.php
@@ -8,17 +8,21 @@ class Task10
     {
         $result = [];
         if (is_int($num)) {
-            array_push($result, $num);
-            while ($num != 1) {
-                if ($num % 2 == 0) {
-                    $num = $num / 2;
-                } else {
-                    $num = $num * 3 + 1;
-                }
+            if ($num <= 0) {
+                throw new \InvalidArgumentException('Введённое значение должно быть целым положительным числом');
+            } else {
                 array_push($result, $num);
-            }
+                while ($num != 1) {
+                    if ($num % 2 == 0) {
+                        $num = $num / 2;
+                    } else {
+                        $num = $num * 3 + 1;
+                    }
+                    array_push($result, $num);
+                }
 
-            return $result;
+                return $result;
+            }
         } else {
             throw new \InvalidArgumentException('Введённое значение должно быть целым числом');
         }
@@ -26,7 +30,7 @@ class Task10
 
     public function main(int $num): array
     {
-        $result = null;
+        $result = [];
 
         try {
             $result = $this->check($num);
